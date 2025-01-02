6d62442b1c1c4552e6a3deb45afe07dd4bc96a14
Herdeny <1653146952@qq.com>
2025-01-03 00:00:10 +0800

    接口汉化

--- src/main/java/com/github/herdeny/webmedicalmanager/controller/DoctorController.java
+++ src/main/java/com/github/herdeny/webmedicalmanager/controller/DoctorController.java
@@ -20,37 +20,37 @@ public class DoctorController {
     @Autowired
     private DoctorService doctorService;
 
-    //通过医生序号查询医生信息
+    ///通过医生序号查询医生信息
     @GetMapping("/doctorInfo/byCode")
     public Result<Doctor> doctorInfoByCode(Integer doctorCode) {
         return Result.success(doctorService.selectDoctorByCode(doctorCode));
     }
 
-    //通过医生名字查询医生信息（模糊查询 %name%）
+    ///通过医生名字查询医生信息（模糊查询 %name%）
     @GetMapping("/doctorInfo/byName")
     public Result<List<Doctor>> doctorInfoByName(String doctorName) {
         return Result.success(doctorService.selectDoctorByName(doctorName));
     }
 
-    //通过医生科室号查询医生信息
+    ///通过医生科室号查询医生信息
     @GetMapping("/doctorInfo/byDepartCode")
     public Result<List<Doctor>> doctorInfoByDepartmentCode(Integer departmentCode) {
         return Result.success(doctorService.selectDoctorByDepartmentCode(departmentCode));
     }
 
-    //通过医生科室名字查询医生信息
+    ///通过医生科室名字查询医生信息
     @GetMapping("/doctorInfo/byDepartName")
     public Result<List<Doctor>> doctorInfoByDepartmentName(String departmentName) {
         return Result.success(doctorService.selectDoctorByDepartmentName(departmentName));
     }
 
-    //获取全部医生信息
+    ///获取全部医生信息
     @GetMapping("/doctorInfo/all")
     public Result<List<Doctor>> doctorInfoAll() {
         return Result.success(doctorService.selectAllDoctor());
     }
 
-    //获取医生信息分页
+    ///获取医生信息分页
     @GetMapping("/doctorInfo/all/page")
     public Result<PageBean<Doctor>> doctorInfoAllPage(Integer page, Integer size) {
         return Result.success(doctorService.selectAllDoctorPage(page, size));
--- src/main/java/com/github/herdeny/webmedicalmanager/controller/UserController.java
+++ src/main/java/com/github/herdeny/webmedicalmanager/controller/UserController.java
@@ -26,8 +26,8 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    //注册
-    // 204-用户名已存在
+    ///注册
+    /// 204-用户名已存在
     @PostMapping("/register")
     public Result register(@Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String username, @Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String password, @Pattern(regexp = "^1[3456789]\\d{9}$") String phone) {
         //查询用户
@@ -40,9 +40,9 @@ public class UserController {
         } else return Result.fail(204);
     }
 
-    //找回密码
-    // 201-用户名手机号不匹配
-    // 202-用户名不存在
+    ///找回密码
+    /// 201-用户名手机号不匹配
+    /// 202-用户名不存在
     @PostMapping("/recovery")
     public Result recovery(@Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String username, @Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String password, @Pattern(regexp = "^1[3456789]\\d{9}$") BigInteger phone) {
         //查询用户
@@ -56,9 +56,9 @@ public class UserController {
         } else return Result.fail(202);
     }
 
-    //登录
-    // 202-用户名不存在
-    // 203-密码错误
+    ///登录
+    /// 202-用户名不存在
+    /// 203-密码错误
     @PostMapping("/login")
     public Result login(@Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String username, @Pattern(regexp = "^[a-zA-Z0-9_]{5,16}$") String password) {
         //查询用户
@@ -81,7 +81,7 @@ public class UserController {
         }
     }
 
-    //获取当前用户信息
+    ///获取当前用户信息
     @GetMapping("/userInfo/current")
     public Result<User> userInfoCurrent() {
         Map<String, Object> userMap = ThreadLocalUtil.get();
@@ -90,35 +90,35 @@ public class UserController {
         return Result.success(u);
     }
 
-    //获取全部用户信息
+    ///获取全部用户信息
     @GetMapping("/userInfo/all")
     public Result<List<User>> userInfoAll() {
         List<User> users = userService.selectAllUser();
         return Result.success(users);
     }
 
-    //分页查询用户信息
+    ///分页查询用户信息
     @GetMapping("/userInfo/all/page")
     public Result<PageBean<User>> userInfoAllPage(Integer page, Integer size) {
         PageBean<User> users = userService.selectAllUserByPage(page, size);
         return Result.success(users);
     }
 
-    //根据用户编号查询用户信息
+    ///根据用户编号查询用户信息
     @GetMapping("/userInfo/byCode")
     public Result<User> userInfoByCode(int code) {
         User u = userService.selectUserByCode(code);
         return Result.success(u);
     }
 
-    //根据用户名查询用户信息
+    ///根据用户名查询用户信息
     @GetMapping("/userInfo/byUsername")
     public Result<User> userInfoByUsername(String username) {
         User u = userService.selectUserByUsername(username);
         return Result.success(u);
     }
 
-    //更新用户信息
+    ///更新用户信息
     @PutMapping("/update")
     public Result<User> update(@RequestBody User user) {
         //这里传过来的密码使用户输入的明文密码需要加密为MD5
@@ -128,8 +128,8 @@ public class UserController {
         return Result.success(user);
     }
 
-    //删除用户
-    // 202-用户名不存在
+    ///删除用户
+    /// 202-用户名不存在
     @DeleteMapping("/delete")
     public Result delete(String username) {
         User u = userService.selectUserByUsername(username);
