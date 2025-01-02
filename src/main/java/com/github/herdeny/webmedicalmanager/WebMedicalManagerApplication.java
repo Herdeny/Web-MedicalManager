66fa5df34595405a8d7b343227592621a89edacc
Herdeny <1653146952@qq.com>
2025-01-02 08:23:35 +0800

    initial commit

---
+++ src/main/java/com/github/herdeny/webmedicalmanager/WebMedicalManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.herdeny.webmedicalmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMedicalManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebMedicalManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/herdeny/webmedicalmanager/WebMedicalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.herdeny.webmedicalmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMedicalManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
